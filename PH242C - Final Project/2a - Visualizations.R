5629317efa1477188b92770fddb42c9ff9b304cf
Kunal Mishra <kmishra9@berkeley.edu>
2018-12-04 03:08:28 -0800

    Plots and GEE code finished

--- PH242C - Final Project/2a - Visualizations.R
+++ PH242C - Final Project/2a - Visualizations.R
@@ -19,30 +19,73 @@ absentee_1617_limited_weekly_rates = fread(file = paste0(long_peak_weekly_rates_
 
 # Plot 1 
 ggplot(data=absentee_1617_limited_weekly_rates, mapping=aes(x=shifted_week, y=weekly_absence_rate, group=school_ID, color=as.factor(intervention))) + 
-  geom_line()
+  geom_line() +
+  ggtitle(label = "Longitudinal Absence Rates by School during 2016-17 peak influenza season") +
+  xlab("School Week during Flu Season") + 
+  ylab("Absence Rate (Absences per student-school-week)") + 
+  scale_color_discrete(name=NULL, labels=c("Control", "Intervention")) +
+  theme(legend.justification = c(1,1), legend.position = c(1,1))
 
 ggsave(filename=here("Flu-Absenteeism", "PH242C - Final Project", "2a-P1-LineSchoolGroupedPlot.png"))
 
 # Plot 2
 ggplot(data=absentee_1617_limited_weekly_rates, mapping=aes(x=shifted_week, y=weekly_absence_rate, group=school_ID, color=as.factor(intervention))) + 
-  geom_smooth(se = FALSE)
+  geom_smooth(se = FALSE) +
+  ggtitle(label = "Longitudinal Absence Rates by School during 2016-17 peak influenza season") +
+  xlab("School Week during Flu Season") + 
+  ylab("Absence Rate (Absences per student-school-week)") + 
+  scale_color_discrete(name=NULL, labels=c("Control", "Intervention")) +
+  theme(legend.justification = c(1,1), legend.position = c(1,1))
 
 ggsave(filename=here("Flu-Absenteeism", "PH242C - Final Project", "2a-P2-SmoothSchoolGroupedPlot.png"))
 
 # Plot 3
 ggplot(data=absentee_1617_limited_weekly_rates, mapping=aes(x=shifted_week, y=weekly_absence_rate, group=intervention, color=as.factor(intervention))) + 
-  geom_smooth(se = FALSE)
+  geom_smooth(se = FALSE) + 
+  ggtitle(label = "Longitudinal Absence Rates by School District during 2016-17 peak influenza season") +
+  xlab("School Week during Flu Season") + 
+  ylab("Absence Rate (Absences per student-school-week)") + 
+  scale_color_discrete(name=NULL, labels=c("Control", "Intervention")) +
+  theme(legend.justification = c(1,1), legend.position = c(1,1))
 
 ggsave(filename=here("Flu-Absenteeism", "PH242C - Final Project", "2a-P3-SmoothQuadraticInterventionGroupedPlot.png"))
 
 # Plot 4
 ggplot(data=absentee_1617_limited_weekly_rates, mapping=aes(x=shifted_week, y=weekly_absence_rate, group=intervention, color=as.factor(intervention))) + 
-  geom_smooth(se = FALSE, method="lm")
+  geom_smooth(se = FALSE, method="lm") + 
+  ggtitle(label = "Linear Fit of Longitudinal Absence Rates by School during 2016-17 peak influenza season") +
+  xlab("School Week during Flu Season") + 
+  ylab("Absence Rate (Absences per student-school-week)") + 
+  scale_color_discrete(name=NULL, labels=c("Control", "Intervention")) +
+  theme(legend.justification = c(1,1), legend.position = c(1,1))
 
 ggsave(filename=here("Flu-Absenteeism", "PH242C - Final Project", "2a-P4-SmoothLinearInterventionGroupedPlot.png"))
 
 # Plot 5
 ggplot(data=absentee_1617_limited_weekly_rates) + 
-  geom_histogram(mapping=aes(x=weekly_absence_rate))
-
+  geom_histogram(mapping=aes(x=weekly_absence_rate), color="black") +
+  ggtitle(label = "Distribution of weekly absence rates during 2016-17 peak influenza season") +
+  xlab("Absence Rate (Absences per student-school-week)") +
+  ylab("Count") 
+  
 ggsave(filename=here("Flu-Absenteeism", "PH242C - Final Project", "2a-P5-HistAbsenceRatePlot.png"))
+
+# Distribution has mean=0.2340408, SD=0.1128782
+
+# Plot 6
+ggplot(data=absentee_1617_limited_weekly_rates, mapping=aes(x=weekly_absence_rate, fill=as.factor(intervention))) + 
+  geom_histogram(color="black") +
+  facet_grid(~ intervention) + 
+  ggtitle(label = "Distribution of weekly absence rates by school district during 2016-17 peak influenza season") +
+  xlab("Absence Rate (Absences per student-school-week)") +
+  ylab("Count") +
+  scale_fill_discrete(name=NULL, labels=c("Control", "Intervention")) +
+  theme(legend.justification = c(1,1), legend.position = c(1,1),
+        strip.text.x = element_blank())
+
+ggsave(filename=here("Flu-Absenteeism", "PH242C - Final Project", "2a-P6-HistFacetAbsenceRatePlot.png"))
+
+# Intervention distribution has mean=0.2211697, SD=0.1125307
+# Control distribution has mean=0.2522119, SD=0.1110833
+
+
--- PH242C - Final Project/2b - GEE.R
+++ PH242C - Final Project/2b - GEE.R
@@ -13,4 +13,97 @@ source(here::here("Flu-Absenteeism", "PH242C - Final Project", "0 - Config.R"))
 
 absentee_1617_limited_weekly_rates = fread(file = paste0(long_peak_weekly_rates_prefix, ".csv")) %>% as.tibble()
 
-gee_fit_unadj = gee(formula = weekly_absence_rate ~ shifted_week + intervention + shifted_week*intervention)
+#################################################################################
+# Conduct unadjusted and adjusted analyses
+#################################################################################
+
+gee_fit_unadj = gee(
+  formula = weekly_absence_rate ~ shifted_week + intervention + shifted_week * intervention,
+  id = school_ID,
+  data = absentee_1617_limited_weekly_rates,
+  family = "gaussian",
+  corstr = "exchangeable",
+  Mv = 1
+)
+
+gee_fit_adj = gee(
+  formula = weekly_absence_rate ~ shifted_week + intervention + per.freelunch + shifted_week * intervention,
+  id = school_ID,
+  data = absentee_1617_limited_weekly_rates,
+  family = "gaussian",
+  corstr = "exchangeable",
+  Mv = 1
+)
+
+#################################################################################
+# Using only weeks 1-4, where the absence rates peak (trying to remove some noise because we're working with a inherently limited linear model)
+
+absentee_1617_limited_weekly_rates_w5max = absentee_1617_limited_weekly_rates %>% filter(shifted_week <= 5)
+
+gee_fit_unadj_w5max = gee(
+  formula = weekly_absence_rate ~ shifted_week + intervention + shifted_week * intervention,
+  id = school_ID,
+  data = absentee_1617_limited_weekly_rates_w5max,
+  family = "gaussian",
+  corstr = "exchangeable",
+  Mv = 1
+)
+
+gee_fit_adj_w5max = gee(
+  formula = weekly_absence_rate ~ shifted_week + intervention + per.freelunch + shifted_week * intervention,
+  id = school_ID,
+  data = absentee_1617_limited_weekly_rates_w5max,
+  family = "gaussian",
+  corstr = "exchangeable",
+  Mv = 1
+)
+
+#################################################################################
+# Conducting all analyses with an AR-M(1) correlation structure
+
+gee_fit_unadj = gee(
+  formula = weekly_absence_rate ~ shifted_week + intervention + shifted_week * intervention,
+  id = school_ID,
+  data = absentee_1617_limited_weekly_rates,
+  family = "gaussian",
+  corstr = "AR-M",
+  Mv = 1
+)
+
+gee_fit_adj = gee(
+  formula = weekly_absence_rate ~ shifted_week + intervention + per.freelunch + shifted_week * intervention,
+  id = school_ID,
+  data = absentee_1617_limited_weekly_rates,
+  family = "gaussian",
+  corstr = "AR-M",
+  Mv = 1
+)
+
+gee_fit_unadj_w5max_ = gee(
+  formula = weekly_absence_rate ~ shifted_week + intervention + shifted_week * intervention,
+  id = school_ID,
+  data = absentee_1617_limited_weekly_rates_w5max,
+  family = "gaussian",
+  corstr = "AR-M",
+  Mv = 1
+)
+
+gee_fit_adj_w5max = gee(
+  formula = weekly_absence_rate ~ shifted_week + intervention + per.freelunch + shifted_week * intervention,
+  id = school_ID,
+  data = absentee_1617_limited_weekly_rates_w5max,
+  family = "gaussian",
+  corstr = "AR-M",
+  Mv = 1
+)
+
+
+#################################################################################
+# Save fit coefficients
+#################################################################################
+
+write_rds(x = gee_fit_unadj %>% summary %>% coef, path = here("Flu-Absenteeism", "PH242C - Final Project", "2b-F1-GEE-unadj.RDS"))      # F1 stands for Fit 1
+write_rds(x = gee_fit_adj   %>% summary %>% coef, path = here("Flu-Absenteeism", "PH242C - Final Project", "2b-F2-GEE-adj.RDS"))
+
+write_rds(x = gee_fit_unadj_w5max %>% summary %>% coef, path = here("Flu-Absenteeism", "PH242C - Final Project", "2b-F3-GEE-w5max-unadj.RDS"))
+write_rds(x = gee_fit_adj_w5max   %>% summary %>% coef, path = here("Flu-Absenteeism", "PH242C - Final Project", "2b-F4-GEE-w5max-adj.RDS"))
