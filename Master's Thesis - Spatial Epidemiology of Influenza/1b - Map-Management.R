43361cf397c2d8fec9c3994766c3e62d6bcd8d18
Kunal Mishra <kmishra9@berkeley.edu>
2019-01-28 08:28:41 -0800

    Additional progress on Map management

--- Master's Thesis - Spatial Epidemiology of Influenza/1b - Map-Management.R
+++ Master's Thesis - Spatial Epidemiology of Influenza/1b - Map-Management.R
@@ -2,7 +2,14 @@
 # Master's Thesis - Spatial Epidemiology of Absenteeism 
 # Shoo the Flu Evaluation
 # 2012-2017 Spatial Epidemiology Analysis
-# File for generating maps
+# Transforms and cleans shape file to be ggplot-friendly and cleans data to include only relevant study schools
+
+# Documentation
+# LEAID Documentation: https://nces.ed.gov/ccd/pdf/sdf101agen.pdf
+# SABS-1516 Documentation: https://nces.ed.gov/programs/edge/docs/EDGE_SABS_2015_2016_TECHDOC.pdf
+# NCES_ID Search: https://nces.ed.gov/ccd/districtsearch/
+# School Name References: https://www.ousd.org/schools and https://www.wccusd.net/domain/96
+# 5 Oakland schools closed in 2013 https://oaklandnorth.net/2011/10/27/ousd-board-votes-to-close-five-elementary-schools-at-a-contentious-meeting/
 ################################################################################
 source(here::here("Flu-Absenteeism", "Master's Thesis - Spatial Epidemiology of Influenza", "0 - Config.R"))
 
@@ -10,9 +17,10 @@ source(here::here("Flu-Absenteeism", "Master's Thesis - Spatial Epidemiology of 
 # Import school shape data & downsize
 ################################################################################
 
+# TODO: Transition to sf
 school_shapes = readOGR(dsn=school_shapes_dir, layer=school_shapes_layer)
 
-# Declare NCES_IDs (leaID) documented here: https://nces.ed.gov/ccd/districtsearch/, and filter by district
+# Declare NCES_IDs (LEAID) documented above and filter by district
 OUSD_NCES_ID = "0628050"
 WCCSD_NCES_ID = "0632550"
 
@@ -22,7 +30,11 @@ select_WCCSD_schools = school_shapes[["leaid"]] %in% WCCSD_NCES_ID
 OUSD_school_shapes = school_shapes[select_OUSD_schools,]
 WCCSD_school_shapes = school_shapes[select_WCCSD_schools,]
 
-# Find all school names within the absentee data set for both districts and compare them to school names from the shape file
+################################################################################
+# Match school names across absentee and school shape data 
+################################################################################
+
+# School names from the absentee data set for both districts
 absentee_school_names = read_rds(path = school_names_path)
 OUSD_absentee_school_names = absentee_school_names$OUSD_school_names
 WCCSD_absentee_school_names = absentee_school_names$WCCSD_school_names
@@ -33,12 +45,12 @@ WCCSD_found = tolower(WCCSD_absentee_school_names) %in% tolower(WCCSD_school_sha
 OUSD_found_names = OUSD_absentee_school_names[OUSD_found]
 WCCSD_found_names = WCCSD_absentee_school_names[WCCSD_found]
 
+# School names from the absentee data set that did not have an exact match 
 OUSD_not_found_names = OUSD_absentee_school_names[!OUSD_found]
 WCCSD_not_found_names = WCCSD_absentee_school_names[!WCCSD_found]
 
-# For each school name that doesn't have an exact match in the opposing shape file, map the name to an alias that *does* appear 
-# School Name References: https://www.ousd.org/schools and https://www.wccusd.net/domain/96
-# 5 Oakland schools closed https://oaklandnorth.net/2011/10/27/ousd-board-votes-to-close-five-elementary-schools-at-a-contentious-meeting/
+# Mapping schools without an exact match to an alias that does appear 
+
 
 OUSD_not_found_aliases = list(
   "ASCEND K-8"                         = str_subset(string = OUSD_school_shapes$schnam, pattern = "ASCEND"),
@@ -68,42 +80,61 @@ OUSD_not_found_aliases = list(
 WCCSD_not_found_aliases = list(
   "Chavez Elementary" = str_subset(string = WCCSD_school_shapes$schnam, pattern = "Chavez"),
   "Downer Elementary" = str_subset(string = WCCSD_school_shapes$schnam, pattern = "Downer"),
-  "King Elementary" = str_subset(string = WCCSD_school_shapes$schnam, pattern = "King"),               # "Martin Luther King Jr. Elementary" isn't on the WCCSD school directory (King Elementary is) but there is no other pattern matching "King" in WCCSD_school_shapes
+  "King Elementary" = str_subset(string = WCCSD_school_shapes$schnam, pattern = "King"),
   "Montalvin Elementary" = str_subset(string = WCCSD_school_shapes$schnam, pattern = "Montalvin")
 )
 
 assert_that(all(names(OUSD_not_found_aliases) %in% OUSD_not_found_names))
 assert_that(all(names(WCCSD_not_found_aliases) %in% WCCSD_not_found_names))
 
-################################################################################
-# Merge in statistical inputs and retrieve full location
-################################################################################
+# All study schools - the combination of exact matches and aliases 
+select_OUSD_study_schools = tolower(OUSD_school_shapes$schnam) %in% tolower(c(OUSD_found_names, OUSD_not_found_aliases))
+select_WCCSD_study_schools = tolower(WCCSD_school_shapes$schnam) %in% tolower(c(WCCSD_found_names, WCCSD_not_found_aliases))
 
-# NOTE: Both districts have a "Lincoln Elementary" ... need to find a way to add a dist.n column to the shapefile depending on its longitude (north, dist.n=0, etc.)
+OUSD_study_school_shapes = OUSD_school_shapes[select_OUSD_study_schools,]
+WCCSD_study_school_shapes = WCCSD_school_shapes[select_WCCSD_study_schools,]
 
-# https://stackoverflow.com/questions/10212956/subset-spatialpolygonsdataframe
-
-# Check accuracy : all WCC schools are above UC Berkeley's latitude (larger) and all OUSD schools are below
-
-study_school_locations_input_1 = study_school_shapes %>% 
+# Transform into mappable dataframe & add absentee_alias column for school names that appear in the absentee dataset (while id contains names that appear in shape file dataset)
+OUSD_study_school_shapes_longlat = OUSD_study_school_shapes %>% 
   spTransform(CRSobj = CRS("+proj=longlat +datum=WGS84")) %>% 
-  tidy()
+  tidy(region = "schnam") %>%
+  mutate(absentee_alias = id, dist.n = 1)
+  
+WCCSD_study_school_shapes_longlat = WCCSD_study_school_shapes %>% 
+  spTransform(CRSobj = CRS("+proj=longlat +datum=WGS84")) %>% 
+  tidy(region = "schnam") %>% 
+  mutate(absentee_alias = id, dist.n = 0)
 
-################################################################################
-# Filter by school year
-################################################################################
+# Update values in absentee_alias column
+for (absentee_school_name in names(OUSD_not_found_aliases)) {
+  shape_school_name = OUSD_not_found_aliases[[absentee_school_name]]
+  
+  if (shape_school_name != "") {
+    OUSD_study_school_shapes_longlat = OUSD_study_school_shapes_longlat %>% 
+      mutate(absentee_alias = str_replace(string = absentee_alias, pattern = shape_school_name, replacement = absentee_school_name))
+  }
+}
+
+for (absentee_school_name in names(WCCSD_not_found_aliases)) {
+  shape_school_name = WCCSD_not_found_aliases[[absentee_school_name]]
+  
+  if (shape_school_name != "") {
+    WCCSD_study_school_shapes_longlat = WCCSD_study_school_shapes_longlat %>% 
+      mutate(absentee_alias = str_replace(string = absentee_alias, pattern = shape_school_name, replacement = absentee_school_name))
+  }
+}
 
 ################################################################################
 # Create the base ggmap layer 
 ################################################################################
 
-sf_east_bay_map = get_map(
-  location = UCB_coords,
-  source = "google",
-  color = "color",
-  zoom = 9
-)
 
-europe <- c(left = -12, bottom = 35, right = 30, top = 63)
-get_stamenmap(europe, zoom = 5) %>% ggmap()
 
+
+
+################################################################################
+# Export several pieces of relevant mapping data
+################################################################################
+
+write_csv(x = OUSD_study_school_shapes_longlat, path = paste0(project_dir, "1b-OUSD-study-school-shapes-longlat.csv"))
+write_csv(x = WCCSD_study_school_shapes_longlat, path = paste0(project_dir, "1b-WCCSD-study-school-shapes-longlat.csv"))
